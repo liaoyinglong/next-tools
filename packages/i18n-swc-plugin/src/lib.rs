a7ee0ccd3eb669f44f119718478f02c6ca2134fe
liaoyinglong <vigossliao@gmail.com>
2022-09-12 20:45:19 +0800

    :construction: tpl with variable , step two

--- packages/i18n-swc-plugin/src/lib.rs
+++ packages/i18n-swc-plugin/src/lib.rs
@@ -1,13 +1,17 @@
-use swc_core::common::util::take::Take;
-use swc_core::{
-    common::DUMMY_SP,
-    ecma::{
-        ast::{CallExpr, Expr, ExprStmt, Program, TaggedTpl},
-        visit::{as_folder, FoldWith, VisitMut},
-    },
-    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
-};
-
+use swc_core::common::DUMMY_SP;
+use swc_core::ecma::ast::CallExpr;
+use swc_core::ecma::ast::Expr;
+use swc_core::ecma::ast::ExprStmt;
+use swc_core::ecma::ast::ObjectLit;
+use swc_core::ecma::ast::Program;
+use swc_core::ecma::ast::Prop;
+use swc_core::ecma::ast::PropOrSpread;
+use swc_core::ecma::ast::TaggedTpl;
+use swc_core::ecma::visit::as_folder;
+use swc_core::ecma::visit::FoldWith;
+use swc_core::ecma::visit::VisitMut;
+use swc_core::plugin::plugin_transform;
+use swc_core::plugin::proxies::TransformPluginProgramMetadata;
 use swc_ecma_utils::ExprFactory;
 
 // static PLUGIN_NAME: &str = "i18n_swc_plugin";
@@ -41,21 +45,39 @@ impl VisitMut for TransformVisitor {
                     }
                 } else {
                     let mut first_arg = String::from("");
+                    let mut props = Vec::with_capacity(tpl.exprs.len());
                     for index in 0..args_len {
                         let i = index / 2;
                         if index % 2 == 0 {
                             if let Some(q) = tpl.quasis.get_mut(i) {
+                                // normal string in temple
                                 first_arg.push_str(q.raw.to_string().as_str());
                             }
                         } else if let Some(e) = tpl.exprs.get_mut(i) {
+                            // variable in temple
                             first_arg.push_str("{");
-                            first_arg
-                                .push_str(e.take().as_ident().unwrap().sym.to_string().as_str());
+                            first_arg.push_str(e.as_ident().unwrap().sym.to_string().as_str());
                             first_arg.push_str("}");
+
+                            if let Some(ident) = e.as_ident() {
+                                props.push(PropOrSpread::Prop(Box::new(Prop::Shorthand(
+                                    ident.clone(),
+                                ))));
+                            }
                         }
                     }
+                    // case: first arg like : Attachment {name} saved
                     args.push(first_arg.as_arg());
+                    // case: second arg like : { name }
+                    args.push(
+                        Expr::Object(ObjectLit {
+                            span: DUMMY_SP,
+                            props,
+                        })
+                        .as_arg(),
+                    );
                 }
+                // replace with new call expression
                 n.expr = Box::new(Expr::Call(CallExpr {
                     args,
                     callee: tag.clone().as_callee(),
--- packages/i18n-swc-plugin/tests/fixture/t_function/normal/output.js
+++ packages/i18n-swc-plugin/tests/fixture/t_function/normal/output.js
@@ -1,11 +1,15 @@
 import { t } from '@lyl/i18next';
-
 // should transform
 t("Refresh inbox");
-t("Refresh inbox");
-
-t("Attachment {name} saved");
-// t(`Attachment ${name} saved`);
-
+t("Attachment {name} saved", {
+    name
+});
+// t`Attachment ${name} saved ${name}`;
+t("Attachment {name} saved {name2}", {
+    name,
+    name2
+});
 // should not transform
+t("Refresh inbox");
+t(`Attachment ${name} saved`);
 raw`Refresh inbox`;

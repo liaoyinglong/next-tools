12c14c07242288b044fba9e117f6eb7d29cf689b
liaoyinglong <vigossliao@gmail.com>
2022-09-14 11:46:34 +0800

    :recycle: make variable name More meaningful

--- packages/i18n-swc-plugin/src/t_function/visitor.rs
+++ packages/i18n-swc-plugin/src/t_function/visitor.rs
@@ -1,11 +1,11 @@
 use swc_core::common::DUMMY_SP;
-use swc_core::ecma::ast::ObjectLit;
 use swc_core::ecma::ast::Prop;
 use swc_core::ecma::ast::PropOrSpread;
 use swc_core::ecma::ast::TaggedTpl;
 use swc_core::ecma::ast::{CallExpr, KeyValueProp};
 use swc_core::ecma::ast::{Expr, PropName};
 use swc_core::ecma::ast::{ExprStmt, Ident};
+use swc_core::ecma::ast::{ObjectLit, Tpl};
 use swc_core::ecma::visit::VisitMut;
 use swc_ecma_utils::ExprFactory;
 
@@ -44,38 +44,38 @@ impl VisitMut for TransformVisitor {
                         args.push(q.raw.clone().as_arg())
                     }
                 } else {
-                    let mut first_arg = String::from("");
+                    let mut msg_id = String::from("");
+                    let mut msg_ids = vec![];
                     let mut props = Vec::with_capacity(tpl.exprs.len());
-                    let mut propstrs = vec![];
                     for index in 0..args_len {
                         let i = index / 2;
                         if index % 2 == 0 {
                             if let Some(q) = tpl.quasis.get_mut(i) {
                                 // normal string in temple
-                                first_arg.push_str(q.raw.to_string().as_str());
+                                msg_id.push_str(q.raw.to_string().as_str());
                             }
                         } else if let Some(e) = tpl.exprs.get_mut(i) {
                             let e = &**e;
                             // variable in temple
-                            first_arg.push_str("{");
+                            msg_id.push_str("{");
 
                             let key;
-                            let mut can_add_to_props = true;
+                            let mut should_push = true;
                             match e {
                                 Expr::Ident(ident) => {
                                     key = PropName::Ident(ident.clone());
-                                    first_arg.push_str(ident.sym.to_string().as_str());
+                                    msg_id.push_str(ident.sym.to_string().as_str());
                                     let ident_name = ident.sym.to_string();
                                     // if already have current name, should not add it again
-                                    can_add_to_props = !propstrs.contains(&ident_name);
-                                    propstrs.push(ident_name);
+                                    should_push = !msg_ids.contains(&ident_name);
+                                    msg_ids.push(ident_name);
                                 }
                                 _ => {
                                     key = PropName::Num(i.into());
-                                    first_arg.push_str(i.to_string().as_str());
+                                    msg_id.push_str(i.to_string().as_str());
                                 }
                             }
-                            if can_add_to_props {
+                            if should_push {
                                 props.push(PropOrSpread::Prop(Box::new(Prop::KeyValue(
                                     KeyValueProp {
                                         key,
@@ -83,11 +83,11 @@ impl VisitMut for TransformVisitor {
                                     },
                                 ))));
                             }
-                            first_arg.push_str("}");
+                            msg_id.push_str("}");
                         }
                     }
                     // case: first arg like : Attachment {name} saved
-                    args.push(first_arg.as_arg());
+                    args.push(msg_id.as_arg());
                     // case: second arg like : { name }
                     args.push(
                         Expr::Object(ObjectLit {
@@ -105,34 +105,34 @@ impl VisitMut for TransformVisitor {
                     type_args: None,
                 }));
             }
-            Expr::Call(call_expr) => {
-                if !is_t_function_call(call_expr.callee.as_ident()) {
-                    return;
-                }
-
-                // more args
-                if call_expr.args.len() != 1 {
-                    return;
-                }
-
-                // if have template literal, should replace it
-                let should_transform = call_expr.args.iter().any(|arg| match *arg.expr {
-                    Expr::Tpl(tpl) => true,
-                    _ => false,
-                });
-                if !should_transform {
-                    return;
-                }
-
-                // replace with new call expression
-                n.expr = Box::new(Expr::Call(CallExpr {
-                    args: call_expr.args.clone(),
-                    callee: call_expr.callee.clone().as_callee(),
-                    span: DUMMY_SP,
-                    type_args: None,
-                }));
-            }
-
+            // Expr::Call(call_expr) => {
+            //     if !is_t_function_call(call_expr.callee.as_ident()) {
+            //         return;
+            //     }
+            //
+            //     // more args should ignore
+            //     if call_expr.args.len() != 1 {
+            //         return;
+            //     }
+            //     if let Expr::Tpl(tpl) = call_expr.args.first() {}
+            //
+            //     // if have template literal, should replace it
+            //     let should_transform = call_expr.args.iter().any(|arg| match *arg.expr {
+            //         Expr::Tpl(tpl) => true,
+            //         _ => false,
+            //     });
+            //     if !should_transform {
+            //         return;
+            //     }
+            //
+            //     // replace with new call expression
+            //     n.expr = Box::new(Expr::Call(CallExpr {
+            //         args: call_expr.args.clone(),
+            //         callee: call_expr.callee.clone().as_callee(),
+            //         span: DUMMY_SP,
+            //         type_args: None,
+            //     }));
+            // }
             _ => {}
         }
     }

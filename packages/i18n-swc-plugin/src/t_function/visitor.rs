a34ac97eadf4df55638e58d36b09c3f6d4fbe58b
liaoyinglong <vigossliao@gmail.com>
2022-09-14 11:56:53 +0800

    :recycle: extract tpl transformer logic

--- packages/i18n-swc-plugin/src/t_function/visitor.rs
+++ packages/i18n-swc-plugin/src/t_function/visitor.rs
@@ -13,6 +13,51 @@ use crate::TransformVisitor;
 
 static T_FUNCTION_NAME: &str = "t";
 
+fn transform_tpl(tpl: &mut Tpl) -> (String, Vec<PropOrSpread>) {
+    let args_len = tpl.exprs.len() + tpl.quasis.len();
+    let mut msg_id = String::from("");
+    let mut msg_ids = vec![];
+    let mut props = Vec::with_capacity(tpl.exprs.len());
+    for index in 0..args_len {
+        let i = index / 2;
+        if index % 2 == 0 {
+            if let Some(q) = tpl.quasis.get_mut(i) {
+                // normal string in temple
+                msg_id.push_str(q.raw.to_string().as_str());
+            }
+        } else if let Some(e) = tpl.exprs.get_mut(i) {
+            let e = &**e;
+            // variable in temple
+            msg_id.push_str("{");
+
+            let key;
+            let mut should_push = true;
+            match e {
+                Expr::Ident(ident) => {
+                    key = PropName::Ident(ident.clone());
+                    msg_id.push_str(ident.sym.to_string().as_str());
+                    let ident_name = ident.sym.to_string();
+                    // if already have current name, should not add it again
+                    should_push = !msg_ids.contains(&ident_name);
+                    msg_ids.push(ident_name);
+                }
+                _ => {
+                    key = PropName::Num(i.into());
+                    msg_id.push_str(i.to_string().as_str());
+                }
+            }
+            if should_push {
+                props.push(PropOrSpread::Prop(Box::new(Prop::KeyValue(KeyValueProp {
+                    key,
+                    value: Box::new(e.clone()),
+                }))));
+            }
+            msg_id.push_str("}");
+        }
+    }
+    (msg_id, props)
+}
+
 impl VisitMut for TransformVisitor {
     // Implement necessary visit_mut_* methods for actual custom transform.
     // A comprehensive list of possible visitor methods can be found here:
@@ -30,62 +75,11 @@ impl VisitMut for TransformVisitor {
                 if !is_t_function_call(tagged_tpl.tag.as_ident()) {
                     return;
                 }
-
                 let TaggedTpl { tpl, tag, .. } = tagged_tpl;
-
                 // initial args vec
-                let args_len = tpl.exprs.len() + tpl.quasis.len();
-                let mut args = Vec::with_capacity(args_len);
-
-                // case normal tagged template, not have variable in template,
-                // and it should only have one argument
-                if tpl.exprs.is_empty() {
-                    if let Some(q) = tpl.quasis.get(0) {
-                        args.push(q.raw.clone().as_arg())
-                    }
-                } else {
-                    let mut msg_id = String::from("");
-                    let mut msg_ids = vec![];
-                    let mut props = Vec::with_capacity(tpl.exprs.len());
-                    for index in 0..args_len {
-                        let i = index / 2;
-                        if index % 2 == 0 {
-                            if let Some(q) = tpl.quasis.get_mut(i) {
-                                // normal string in temple
-                                msg_id.push_str(q.raw.to_string().as_str());
-                            }
-                        } else if let Some(e) = tpl.exprs.get_mut(i) {
-                            let e = &**e;
-                            // variable in temple
-                            msg_id.push_str("{");
-
-                            let key;
-                            let mut should_push = true;
-                            match e {
-                                Expr::Ident(ident) => {
-                                    key = PropName::Ident(ident.clone());
-                                    msg_id.push_str(ident.sym.to_string().as_str());
-                                    let ident_name = ident.sym.to_string();
-                                    // if already have current name, should not add it again
-                                    should_push = !msg_ids.contains(&ident_name);
-                                    msg_ids.push(ident_name);
-                                }
-                                _ => {
-                                    key = PropName::Num(i.into());
-                                    msg_id.push_str(i.to_string().as_str());
-                                }
-                            }
-                            if should_push {
-                                props.push(PropOrSpread::Prop(Box::new(Prop::KeyValue(
-                                    KeyValueProp {
-                                        key,
-                                        value: Box::new(e.clone()),
-                                    },
-                                ))));
-                            }
-                            msg_id.push_str("}");
-                        }
-                    }
+                let mut args = vec![];
+                if !(tpl.exprs.is_empty()) {
+                    let (msg_id, props) = transform_tpl(tpl);
                     // case: first arg like : Attachment {name} saved
                     args.push(msg_id.as_arg());
                     // case: second arg like : { name }
@@ -96,6 +90,10 @@ impl VisitMut for TransformVisitor {
                         })
                         .as_arg(),
                     );
+                } else if let Some(q) = tpl.quasis.get(0) {
+                    // case normal tagged template, not have variable in template,
+                    // and it should only have one argument
+                    args.push(q.raw.clone().as_arg())
                 }
                 // replace with new call expression
                 n.expr = Box::new(Expr::Call(CallExpr {

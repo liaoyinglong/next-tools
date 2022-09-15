a64c186f9f3a8b251b2d3aba85588f0ef2b19cea
liaoyinglong <vigossliao@gmail.com>
2022-09-15 14:48:18 +0800

    :test_tube: update snapshot

--- packages/i18n-swc-plugin/tests/fixture/trans/normal/output.js
+++ packages/i18n-swc-plugin/tests/fixture/trans/normal/output.js
@@ -1,8 +1,8 @@
 // normal
-<Trans id="Refresh inbox" />
+<Trans id="Refresh inbox" />;
 // with variable
 <Trans id="Attachment {name} saved."/>;
 // Custom id is preserved:
-<Trans id="message.attachment_saved" message="Attachment {name} saved." />
+<Trans id="message.attachment_saved" message="Attachment {name} saved." />;
 //  inline markup
-<Trans id="Read the <0>docs</0>." components={{0: <a href="/docs" />}} />
+<Trans id="Read the <0>docs</0>." components={{0: <a href="/docs" />}} />;

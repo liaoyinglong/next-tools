dd11ab556053cabb82da611ba153c5f013748b45
liaoyinglong <vigossliao@gmail.com>
2022-09-19 10:49:39 +0800

    :wrench: update next.config

--- packages/swc-plugin/example/next-js/next.config.js
+++ packages/swc-plugin/example/next-js/next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   experimental: {
     /**
      * @see https://nextjs.org/docs/advanced-features/compiler#swc-plugins-experimental
      */
-    swcPlugins: [[require.resolve("@scope/swc-plugin")]],
+    swcPlugins: [
+      [require.resolve("@scope/swc-plugin/dist/swc_plugin.wasm"), {}],
+    ],
   },
 };
 

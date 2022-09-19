3fb057f983b2979836c41f8ac4b25f4b8e1202ac
liaoyinglong <vigossliao@gmail.com>
2022-09-19 09:57:56 +0800

    :wrench: update build flow

---
+++ packages/swc-plugin/scripts/build.mjs
@@ -0,0 +1,14 @@
+import { $, echo, fs } from "zx";
+
+await fs.emptyDir("./dist");
+echo("Building...");
+
+await $`cargo build-wasi --release`;
+echo(`Built swc_plugin.wasm`);
+
+await fs.copyFile(
+  `./target/wasm32-wasi/release/s_swc_plugin.wasm`,
+  `./dist/swc_plugin.wasm`
+);
+
+echo(`Copied swc_plugin_bg.wasm to dist`);

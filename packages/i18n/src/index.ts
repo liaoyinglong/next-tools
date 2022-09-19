c30d5a8b74a490dacee27e9b3058b24839a87277
liaoyinglong <vigossliao@gmail.com>
2022-09-19 21:23:49 +0800

    :bug: missing reexport

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,6 +1,6 @@
 import { i18n } from "@lingui/core";
 
-export { I18nProvider } from "@lingui/react";
+export { I18nProvider, Trans } from "@lingui/react";
 export * from "@lingui/core";
 
 export const t = i18n._.bind(i18n);

1d3f810d21eb4d8f60b62ae8cff5eabf2c745b05
liaoyinglong <vigossliao@gmail.com>
2022-09-28 19:33:50 +0800

    fix: 添加英语标识

--- packages/tools/src/i18n/enums.ts
+++ packages/tools/src/i18n/enums.ts
@@ -1,8 +1,11 @@
+// https://gist.github.com/msikma/8912e62ed866778ff8cd
 export enum LocalesEnum {
   //  中文
   zh_CN = "zh_CN",
   // 印尼
   in_ID = "in_ID",
+  // 英文
+  en_US = "en_US",
 }
 
 export enum DetectLocaleEnum {

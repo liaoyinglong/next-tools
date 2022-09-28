98570243974c48f6db404226f0ae2303c1631dc7
liaoyinglong <vigossliao@gmail.com>
2022-09-28 10:30:07 +0800

    feat: 整合next plugin 到独立仓库

--- example/next-js/next.config.js
+++ example/next-js/next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.optimization.minimizer = [];
+    return config;
+  },
   experimental: {
     /**
      * @see https://nextjs.org/docs/advanced-features/compiler#swc-plugins-experimental
@@ -22,5 +26,31 @@ const nextConfig = {
     swcPlugins: [["next-superjson-plugin", {}]],
   },
 };
+const combinedConfig = withDunePresets()({
+  swcMinify: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.optimization.minimizer = [];
+    return config;
+  },
+  experimental: {
+    /**
+     * @see https://nextjs.org/docs/advanced-features/compiler#swc-plugins-experimental
+     */
+    swcPlugins: [["next-superjson-plugin", {}]],
+  },
+  i,
+});
 
-module.exports = withDunePresets()(nextConfig);
+module.exports = combinedConfig;
--- packages/next-plugin/src/index.ts
+++ packages/next-plugin/src/index.ts
@@ -2,7 +2,7 @@ import withTM from "next-transpile-modules";
 import { Options as AutoImportOptions } from "unplugin-auto-import/types";
 import autoImportPlugin from "unplugin-auto-import/webpack";
 import { i18nResourcePlugin } from "@dune2/unplugin";
-
+import { NextConfig } from "next";
 interface Options {
   /**
    * 配置需要走babel/swc编译的依赖
@@ -19,15 +19,10 @@ interface Options {
  */
 export const withDunePresets = (options: Options = {}) => {
   return withDunePresetsImpl;
-  function withDunePresetsImpl(nextConfig: any) {
-    set(nextConfig, ["experimental", "externalDir"], true);
-    set(
-      nextConfig,
-      ["experimental", "swcPlugins"],
-      [[require.resolve("@dune2/swc-plugin"), {}]].concat(
-        nextConfig?.experimental?.swcPlugins
-      )
-    );
+  function withDunePresetsImpl(nextConfig: NextConfig) {
+    updateValue(nextConfig, ["experimental", "externalDir"], true);
+    const defaultSwcPlugins = [[require.resolve("@dune2/swc-plugin"), {}]];
+    updateValue(nextConfig, ["experimental", "swcPlugins"], defaultSwcPlugins);
 
     const combinedConfig = withTM(
       ["@dune2/i18n"].concat(options.transpileModules).filter(Boolean)
@@ -55,12 +50,27 @@ export const withDunePresets = (options: Options = {}) => {
   }
 };
 
-function set(obj: any, path: string[], value: any) {
+/**
+ * 更新对象的值
+ * 值的类型是数组，会合并
+ * 值的类型是对象，会合并
+ * 如果是undefined，会设置为默认值
+ */
+function updateValue(obj: any, path: string[], value: any) {
   const last = path.pop();
   path.forEach((key) => {
     obj = obj[key] || (obj[key] = {});
   });
-  if (obj[last] === undefined) {
+
+  if (Array.isArray(obj[last])) {
+    obj[last] = [...obj[last], ...value];
+    return;
+  }
+  if (typeof obj[last] === "object") {
+    obj[last] = { ...obj[last], ...value };
+    return;
+  }
+  if (typeof obj[last] === "undefined") {
     obj[last] = value;
   }
 }

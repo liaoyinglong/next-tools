b324ef246affc2f2edbd7d83cb49fa84a53083e3
liaoyinglong <vigossliao@gmail.com>
2022-11-30 14:58:33 +0800

    fix: 更新测试快照

--- packages/wasm/js-test/extract.spec.ts
+++ packages/wasm/js-test/extract.spec.ts
@@ -3,6 +3,7 @@ import { extract } from "..";
 
 const str = [
   "t`hello ${name}`",
+  `t({ id: "t.fn.obj.arg", message: "Refresh inbox" })`,
   "<Trans>hello {name2}</Trans>",
   '<Trans id="msg_id1">hello {name2}</Trans>',
   '<Trans id={"msg_id2"}>hello {name2}</Trans>',
@@ -13,18 +14,27 @@ const str = [
 describe("extract", () => {
   it("extract correctly", async () => {
     const result = await extract(str, "test.jsx");
-    expect(result.data.size).toMatchInlineSnapshot("5");
+    expect(result.data.size).toMatchInlineSnapshot("6");
     expect(result.errMsg).toMatchInlineSnapshot(`
       "
         [36m>[0m msg id is not string literal, skip
          ,-[[36;1;4mtest.jsx[0m:1:1]
-       [2m1[0m | t\`hello \${name}\`;<Trans>hello {name2}</Trans>;<Trans id=\\"msg_id1\\">hello {name2}</Trans>;<Trans id={\\"msg_id2\\"}>hello {name2}</Trans>;<Trans id={\`msg_id3\`}>hello {name2}</Trans>;t(\`hello \${name333}\`)
-         : [31;1m                                                                                                                                                                                ^^^^^^^^^^^^^^^^^^^^^[0m
+       [2m1[0m | t\`hello \${name}\`;t({ id: \\"t.fn.obj.arg\\", message: \\"Refresh inbox\\" });<Trans>hello {name2}</Trans>;<Trans id=\\"msg_id1\\">hello {name2}</Trans>;<Trans id={\\"msg_id2\\"}>hello {name2}</Trans>;<Trans id={\`msg_id3\`}>hello {name2}</Trans>;t(\`hello \${name333}\`)
+         : [31;1m                                                                                                                                                                                                                                    ^^^^^^^^^^^^^^^^^^^^^[0m
          \`----
       "
     `);
+
     expect(result.data).toMatchInlineSnapshot(`
       Map {
+        "msg_id2" => {
+          "id": "msg_id2",
+          "messages": "hello {name2}",
+        },
+        "msg_id3" => {
+          "id": "msg_id3",
+          "messages": "hello {name2}",
+        },
         "hello {name2}" => {
           "id": "hello {name2}",
           "messages": "",
@@ -33,17 +43,13 @@ describe("extract", () => {
           "id": "hello {name}",
           "messages": "",
         },
-        "msg_id3" => {
-          "id": "msg_id3",
-          "messages": "hello {name2}",
-        },
         "msg_id1" => {
           "id": "msg_id1",
           "messages": "hello {name2}",
         },
-        "msg_id2" => {
-          "id": "msg_id2",
-          "messages": "hello {name2}",
+        "t.fn.obj.arg" => {
+          "id": "t.fn.obj.arg",
+          "messages": "Refresh inbox",
         },
       }
     `);

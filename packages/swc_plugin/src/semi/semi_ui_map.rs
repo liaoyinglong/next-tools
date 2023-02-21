9756ce8223f3ba503ffba1b6f3853f2803270b92
liaoyinglong <vigossliao@gmail.com>
2023-02-21 11:20:46 +0800

    wip: 排序

--- packages/swc_plugin/src/semi/semi_ui_map.rs
+++ packages/swc_plugin/src/semi/semi_ui_map.rs
@@ -4,192 +4,84 @@ use swc_core::common::collections::AHashMap;
 pub fn get_semi_ui_map() -> AHashMap<String, String> {
     let mut map = AHashMap::default();
     map.insert(
-        "TagGroup".to_string(),
-        "@douyinfe/semi-ui/lib/es/tag/group".to_string(),
-    );
-    map.insert(
-        "Spin".to_string(),
-        "@douyinfe/semi-ui/lib/es/spin".to_string(),
-    );
-    map.insert(
-        "SideSheet".to_string(),
-        "@douyinfe/semi-ui/lib/es/sideSheet".to_string(),
-    );
-    map.insert(
-        "withFormState".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
-    );
-    map.insert(
-        "OverflowList".to_string(),
-        "@douyinfe/semi-ui/lib/es/overflowList".to_string(),
-    );
-    map.insert(
-        "ScrollItem".to_string(),
-        "@douyinfe/semi-ui/lib/es/scrollList/scrollItem".to_string(),
+        "Anchor".to_string(),
+        "@douyinfe/semi-ui/lib/es/anchor".to_string(),
     );
     map.insert(
         "ArrayField".to_string(),
         "@douyinfe/semi-ui/lib/es/form".to_string(),
     );
     map.insert(
-        "TextArea".to_string(),
-        "@douyinfe/semi-ui/lib/es/input/textarea".to_string(),
+        "AutoComplete".to_string(),
+        "@douyinfe/semi-ui/lib/es/autoComplete".to_string(),
     );
     map.insert(
-        "Step".to_string(),
-        "@douyinfe/semi-ui/lib/es/steps/step".to_string(),
+        "Avatar".to_string(),
+        "@douyinfe/semi-ui/lib/es/avatar".to_string(),
     );
     map.insert(
-        "Space".to_string(),
-        "@douyinfe/semi-ui/lib/es/space".to_string(),
+        "AvatarGroup".to_string(),
+        "@douyinfe/semi-ui/lib/es/avatar/avatarGroup".to_string(),
     );
     map.insert(
-        "Anchor".to_string(),
-        "@douyinfe/semi-ui/lib/es/anchor".to_string(),
-    );
-    map.insert(
-        "Dropdown".to_string(),
-        "@douyinfe/semi-ui/lib/es/dropdown".to_string(),
-    );
-    map.insert(
-        "Timeline".to_string(),
-        "@douyinfe/semi-ui/lib/es/timeline".to_string(),
-    );
-    map.insert(
-        "Divider".to_string(),
-        "@douyinfe/semi-ui/lib/es/divider".to_string(),
-    );
-    map.insert(
-        "Upload".to_string(),
-        "@douyinfe/semi-ui/lib/es/upload".to_string(),
-    );
-    map.insert(
-        "LocaleProvider".to_string(),
-        "@douyinfe/semi-ui/lib/es/locale/localeProvider".to_string(),
+        "BackTop".to_string(),
+        "@douyinfe/semi-ui/lib/es/backtop".to_string(),
     );
     map.insert(
         "Badge".to_string(),
         "@douyinfe/semi-ui/lib/es/badge".to_string(),
     );
     map.insert(
-        "InputGroup".to_string(),
-        "@douyinfe/semi-ui/lib/es/input/inputGroup".to_string(),
+        "Banner".to_string(),
+        "@douyinfe/semi-ui/lib/es/banner".to_string(),
     );
     map.insert(
-        "Tooltip".to_string(),
-        "@douyinfe/semi-ui/lib/es/tooltip".to_string(),
+        "Breadcrumb".to_string(),
+        "@douyinfe/semi-ui/lib/es/breadcrumb".to_string(),
     );
     map.insert(
-        "Popover".to_string(),
-        "@douyinfe/semi-ui/lib/es/popover".to_string(),
-    );
-    map.insert(
-        "Tree".to_string(),
-        "@douyinfe/semi-ui/lib/es/tree".to_string(),
-    );
-    map.insert(
-        "List".to_string(),
-        "@douyinfe/semi-ui/lib/es/list".to_string(),
-    );
-    map.insert(
-        "Collapse".to_string(),
-        "@douyinfe/semi-ui/lib/es/collapse".to_string(),
-    );
-    map.insert(
-        "withFormApi".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
+        "Button".to_string(),
+        "@douyinfe/semi-ui/lib/es/button".to_string(),
     );
     map.insert(
         "ButtonGroup".to_string(),
         "@douyinfe/semi-ui/lib/es/button/buttonGroup".to_string(),
     );
     map.insert(
-        "Table".to_string(),
-        "@douyinfe/semi-ui/lib/es/table".to_string(),
+        "Calendar".to_string(),
+        "@douyinfe/semi-ui/lib/es/calendar".to_string(),
     );
     map.insert(
-        "useFormApi".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
-    );
-    map.insert(
-        "DropdownDivider".to_string(),
-        "@douyinfe/semi-ui/lib/es/dropdown/dropdownDivider".to_string(),
-    );
-    map.insert(
-        "Rating".to_string(),
-        "@douyinfe/semi-ui/lib/es/rating".to_string(),
-    );
-    map.insert(
-        "Empty".to_string(),
-        "@douyinfe/semi-ui/lib/es/empty".to_string(),
-    );
-    map.insert(
-        "Input".to_string(),
-        "@douyinfe/semi-ui/lib/es/input".to_string(),
-    );
-    map.insert(
-        "LocaleConsumer".to_string(),
-        "@douyinfe/semi-ui/lib/es/locale/localeConsumer".to_string(),
-    );
-    map.insert(
-        "useFormState".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
+        "Card".to_string(),
+        "@douyinfe/semi-ui/lib/es/card".to_string(),
     );
     map.insert(
         "CardGroup".to_string(),
         "@douyinfe/semi-ui/lib/es/card/cardGroup".to_string(),
     );
     map.insert(
-        "Layout".to_string(),
-        "@douyinfe/semi-ui/lib/es/layout".to_string(),
+        "Carousel".to_string(),
+        "@douyinfe/semi-ui/lib/es/carousel".to_string(),
     );
     map.insert(
-        "Calendar".to_string(),
-        "@douyinfe/semi-ui/lib/es/calendar".to_string(),
-    );
-    map.insert(
-        "Switch".to_string(),
-        "@douyinfe/semi-ui/lib/es/switch".to_string(),
-    );
-    map.insert(
-        "InputNumber".to_string(),
-        "@douyinfe/semi-ui/lib/es/inputNumber".to_string(),
-    );
-    map.insert(
-        "Select".to_string(),
-        "@douyinfe/semi-ui/lib/es/select".to_string(),
-    );
-    map.insert(
-        "DropdownItem".to_string(),
-        "@douyinfe/semi-ui/lib/es/dropdown/dropdownItem".to_string(),
+        "Cascader".to_string(),
+        "@douyinfe/semi-ui/lib/es/cascader".to_string(),
     );
     map.insert(
         "Checkbox".to_string(),
         "@douyinfe/semi-ui/lib/es/checkbox".to_string(),
     );
     map.insert(
-        "SubNav".to_string(),
-        "@douyinfe/semi-ui/lib/es/navigation/SubNav".to_string(),
-    );
-    map.insert(
         "CheckboxGroup".to_string(),
         "@douyinfe/semi-ui/lib/es/checkbox/checkboxGroup".to_string(),
     );
     map.insert(
-        "DropdownMenu".to_string(),
-        "@douyinfe/semi-ui/lib/es/dropdown/dropdownMenu".to_string(),
+        "Col".to_string(),
+        "@douyinfe/semi-ui/lib/es/grid".to_string(),
     );
     map.insert(
-        "SplitButtonGroup".to_string(),
-        "@douyinfe/semi-ui/lib/es/button/splitButtonGroup".to_string(),
-    );
-    map.insert(
-        "TreeSelect".to_string(),
-        "@douyinfe/semi-ui/lib/es/treeSelect".to_string(),
-    );
-    map.insert(
-        "Form".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
+        "Collapse".to_string(),
+        "@douyinfe/semi-ui/lib/es/collapse".to_string(),
     );
     map.insert(
         "Collapsible".to_string(),
@@ -204,16 +96,84 @@ pub fn get_semi_ui_map() -> AHashMap<String, String> {
         "@douyinfe/semi-ui/lib/es/datePicker".to_string(),
     );
     map.insert(
-        "Notification".to_string(),
-        "@douyinfe/semi-ui/lib/es/notification".to_string(),
+        "Descriptions".to_string(),
+        "@douyinfe/semi-ui/lib/es/descriptions".to_string(),
     );
     map.insert(
-        "NavItem".to_string(),
-        "@douyinfe/semi-ui/lib/es/navigation/Item".to_string(),
+        "Divider".to_string(),
+        "@douyinfe/semi-ui/lib/es/divider".to_string(),
     );
     map.insert(
-        "Skeleton".to_string(),
-        "@douyinfe/semi-ui/lib/es/skeleton".to_string(),
+        "Dropdown".to_string(),
+        "@douyinfe/semi-ui/lib/es/dropdown".to_string(),
+    );
+    map.insert(
+        "DropdownDivider".to_string(),
+        "@douyinfe/semi-ui/lib/es/dropdown/dropdownDivider".to_string(),
+    );
+    map.insert(
+        "DropdownItem".to_string(),
+        "@douyinfe/semi-ui/lib/es/dropdown/dropdownItem".to_string(),
+    );
+    map.insert(
+        "DropdownMenu".to_string(),
+        "@douyinfe/semi-ui/lib/es/dropdown/dropdownMenu".to_string(),
+    );
+    map.insert(
+        "Empty".to_string(),
+        "@douyinfe/semi-ui/lib/es/empty".to_string(),
+    );
+    map.insert(
+        "Form".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "Highlight".to_string(),
+        "@douyinfe/semi-ui/lib/es/highlight".to_string(),
+    );
+    map.insert(
+        "Icon".to_string(),
+        "@douyinfe/semi-ui/lib/es/icons".to_string(),
+    );
+    map.insert(
+        "IconButton".to_string(),
+        "@douyinfe/semi-ui/lib/es/iconButton".to_string(),
+    );
+    map.insert(
+        "Image".to_string(),
+        "@douyinfe/semi-ui/lib/es/image".to_string(),
+    );
+    map.insert(
+        "ImagePreview".to_string(),
+        "@douyinfe/semi-ui/lib/es/image".to_string(),
+    );
+    map.insert(
+        "Input".to_string(),
+        "@douyinfe/semi-ui/lib/es/input".to_string(),
+    );
+    map.insert(
+        "InputGroup".to_string(),
+        "@douyinfe/semi-ui/lib/es/input/inputGroup".to_string(),
+    );
+    map.insert(
+        "InputNumber".to_string(),
+        "@douyinfe/semi-ui/lib/es/inputNumber".to_string(),
+    );
+    map.insert(
+        "Layout".to_string(),
+        "@douyinfe/semi-ui/lib/es/layout".to_string(),
+    );
+    map.insert(
+        "List".to_string(),
+        "@douyinfe/semi-ui/lib/es/list".to_string(),
+    );
+    map.insert(
+        "LocaleConsumer".to_string(),
+        "@douyinfe/semi-ui/lib/es/locale/localeConsumer".to_string(),
+    );
+    map.insert(
+        "LocaleProvider".to_string(),
+        "@douyinfe/semi-ui/lib/es/locale/localeProvider".to_string(),
     );
     map.insert(
         "Modal".to_string(),
@@ -224,155 +184,195 @@ pub fn get_semi_ui_map() -> AHashMap<String, String> {
         "@douyinfe/semi-ui/lib/es/navigation".to_string(),
     );
     map.insert(
-        "Progress".to_string(),
-        "@douyinfe/semi-ui/lib/es/progress".to_string(),
+        "NavItem".to_string(),
+        "@douyinfe/semi-ui/lib/es/navigation/Item".to_string(),
     );
     map.insert(
-        "Slider".to_string(),
-        "@douyinfe/semi-ui/lib/es/slider".to_string(),
+        "Notification".to_string(),
+        "@douyinfe/semi-ui/lib/es/notification".to_string(),
     );
     map.insert(
-        "TabPane".to_string(),
-        "@douyinfe/semi-ui/lib/es/tabs/TabPane".to_string(),
-    );
-    map.insert(
-        "Avatar".to_string(),
-        "@douyinfe/semi-ui/lib/es/avatar".to_string(),
-    );
-    map.insert(
-        "Col".to_string(),
-        "@douyinfe/semi-ui/lib/es/grid".to_string(),
-    );
-    map.insert(
-        "Carousel".to_string(),
-        "@douyinfe/semi-ui/lib/es/carousel".to_string(),
-    );
-    map.insert(
-        "IconButton".to_string(),
-        "@douyinfe/semi-ui/lib/es/iconButton".to_string(),
-    );
-    map.insert(
-        "RadioGroup".to_string(),
-        "@douyinfe/semi-ui/lib/es/radio/radioGroup".to_string(),
-    );
-    map.insert(
-        "Descriptions".to_string(),
-        "@douyinfe/semi-ui/lib/es/descriptions".to_string(),
-    );
-    map.insert(
-        "ToastFactory".to_string(),
-        "@douyinfe/semi-ui/lib/es/toast".to_string(),
-    );
-    map.insert(
-        "Transfer".to_string(),
-        "@douyinfe/semi-ui/lib/es/transfer".to_string(),
-    );
-    map.insert(
-        "withField".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
-    );
-    map.insert(
-        "ScrollList".to_string(),
-        "@douyinfe/semi-ui/lib/es/scrollList".to_string(),
-    );
-    map.insert(
-        "TagInput".to_string(),
-        "@douyinfe/semi-ui/lib/es/tagInput".to_string(),
-    );
-    map.insert(
-        "ImagePreview".to_string(),
-        "@douyinfe/semi-ui/lib/es/image".to_string(),
-    );
-    map.insert(
-        "Tag".to_string(),
-        "@douyinfe/semi-ui/lib/es/tag".to_string(),
-    );
-    map.insert(
-        "AutoComplete".to_string(),
-        "@douyinfe/semi-ui/lib/es/autoComplete".to_string(),
-    );
-    map.insert(
-        "Cascader".to_string(),
-        "@douyinfe/semi-ui/lib/es/cascader".to_string(),
-    );
-    map.insert(
-        "BackTop".to_string(),
-        "@douyinfe/semi-ui/lib/es/backtop".to_string(),
-    );
-    map.insert(
-        "TimePicker".to_string(),
-        "@douyinfe/semi-ui/lib/es/timePicker".to_string(),
-    );
-    map.insert(
-        "Banner".to_string(),
-        "@douyinfe/semi-ui/lib/es/banner".to_string(),
-    );
-    map.insert(
-        "Highlight".to_string(),
-        "@douyinfe/semi-ui/lib/es/highlight".to_string(),
-    );
-    map.insert(
-        "Breadcrumb".to_string(),
-        "@douyinfe/semi-ui/lib/es/breadcrumb".to_string(),
-    );
-    map.insert(
-        "Card".to_string(),
-        "@douyinfe/semi-ui/lib/es/card".to_string(),
-    );
-    map.insert(
-        "useFieldState".to_string(),
-        "@douyinfe/semi-ui/lib/es/form".to_string(),
-    );
-    map.insert(
-        "Button".to_string(),
-        "@douyinfe/semi-ui/lib/es/button".to_string(),
-    );
-    map.insert(
-        "Icon".to_string(),
-        "@douyinfe/semi-ui/lib/es/icons".to_string(),
-    );
-    map.insert(
-        "AvatarGroup".to_string(),
-        "@douyinfe/semi-ui/lib/es/avatar/avatarGroup".to_string(),
+        "OverflowList".to_string(),
+        "@douyinfe/semi-ui/lib/es/overflowList".to_string(),
     );
     map.insert(
         "Pagination".to_string(),
         "@douyinfe/semi-ui/lib/es/pagination".to_string(),
     );
     map.insert(
-        "Toast".to_string(),
-        "@douyinfe/semi-ui/lib/es/toast".to_string(),
+        "Popconfirm".to_string(),
+        "@douyinfe/semi-ui/lib/es/popconfirm".to_string(),
+    );
+    map.insert(
+        "Popover".to_string(),
+        "@douyinfe/semi-ui/lib/es/popover".to_string(),
+    );
+    map.insert(
+        "Progress".to_string(),
+        "@douyinfe/semi-ui/lib/es/progress".to_string(),
     );
     map.insert(
         "Radio".to_string(),
         "@douyinfe/semi-ui/lib/es/radio".to_string(),
     );
     map.insert(
-        "Popconfirm".to_string(),
-        "@douyinfe/semi-ui/lib/es/popconfirm".to_string(),
+        "RadioGroup".to_string(),
+        "@douyinfe/semi-ui/lib/es/radio/radioGroup".to_string(),
     );
     map.insert(
-        "Typography".to_string(),
-        "@douyinfe/semi-ui/lib/es/typography".to_string(),
-    );
-    map.insert(
-        "Image".to_string(),
-        "@douyinfe/semi-ui/lib/es/image".to_string(),
+        "Rating".to_string(),
+        "@douyinfe/semi-ui/lib/es/rating".to_string(),
     );
     map.insert(
         "Row".to_string(),
         "@douyinfe/semi-ui/lib/es/grid".to_string(),
     );
     map.insert(
+        "ScrollItem".to_string(),
+        "@douyinfe/semi-ui/lib/es/scrollList/scrollItem".to_string(),
+    );
+    map.insert(
+        "ScrollList".to_string(),
+        "@douyinfe/semi-ui/lib/es/scrollList".to_string(),
+    );
+    map.insert(
+        "Select".to_string(),
+        "@douyinfe/semi-ui/lib/es/select".to_string(),
+    );
+    map.insert(
+        "SideSheet".to_string(),
+        "@douyinfe/semi-ui/lib/es/sideSheet".to_string(),
+    );
+    map.insert(
+        "Skeleton".to_string(),
+        "@douyinfe/semi-ui/lib/es/skeleton".to_string(),
+    );
+    map.insert(
+        "Slider".to_string(),
+        "@douyinfe/semi-ui/lib/es/slider".to_string(),
+    );
+    map.insert(
+        "Space".to_string(),
+        "@douyinfe/semi-ui/lib/es/space".to_string(),
+    );
+    map.insert(
+        "Spin".to_string(),
+        "@douyinfe/semi-ui/lib/es/spin".to_string(),
+    );
+    map.insert(
+        "SplitButtonGroup".to_string(),
+        "@douyinfe/semi-ui/lib/es/button/splitButtonGroup".to_string(),
+    );
+    map.insert(
+        "Step".to_string(),
+        "@douyinfe/semi-ui/lib/es/steps/step".to_string(),
+    );
+    map.insert(
         "Steps".to_string(),
         "@douyinfe/semi-ui/lib/es/steps".to_string(),
+    );
+    map.insert(
+        "SubNav".to_string(),
+        "@douyinfe/semi-ui/lib/es/navigation/SubNav".to_string(),
+    );
+    map.insert(
+        "Switch".to_string(),
+        "@douyinfe/semi-ui/lib/es/switch".to_string(),
+    );
+    map.insert(
+        "TabPane".to_string(),
+        "@douyinfe/semi-ui/lib/es/tabs/TabPane".to_string(),
+    );
+    map.insert(
+        "Table".to_string(),
+        "@douyinfe/semi-ui/lib/es/table".to_string(),
     );
     map.insert(
         "Tabs".to_string(),
         "@douyinfe/semi-ui/lib/es/tabs".to_string(),
     );
     map.insert(
+        "Tag".to_string(),
+        "@douyinfe/semi-ui/lib/es/tag".to_string(),
+    );
+    map.insert(
+        "TagGroup".to_string(),
+        "@douyinfe/semi-ui/lib/es/tag/group".to_string(),
+    );
+    map.insert(
+        "TagInput".to_string(),
+        "@douyinfe/semi-ui/lib/es/tagInput".to_string(),
+    );
+    map.insert(
+        "TextArea".to_string(),
+        "@douyinfe/semi-ui/lib/es/input/textarea".to_string(),
+    );
+    map.insert(
+        "TimePicker".to_string(),
+        "@douyinfe/semi-ui/lib/es/timePicker".to_string(),
+    );
+    map.insert(
+        "Timeline".to_string(),
+        "@douyinfe/semi-ui/lib/es/timeline".to_string(),
+    );
+    map.insert(
+        "Toast".to_string(),
+        "@douyinfe/semi-ui/lib/es/toast".to_string(),
+    );
+    map.insert(
+        "ToastFactory".to_string(),
+        "@douyinfe/semi-ui/lib/es/toast".to_string(),
+    );
+    map.insert(
+        "Tooltip".to_string(),
+        "@douyinfe/semi-ui/lib/es/tooltip".to_string(),
+    );
+    map.insert(
+        "Transfer".to_string(),
+        "@douyinfe/semi-ui/lib/es/transfer".to_string(),
+    );
+    map.insert(
+        "Tree".to_string(),
+        "@douyinfe/semi-ui/lib/es/tree".to_string(),
+    );
+    map.insert(
+        "TreeSelect".to_string(),
+        "@douyinfe/semi-ui/lib/es/treeSelect".to_string(),
+    );
+    map.insert(
+        "Typography".to_string(),
+        "@douyinfe/semi-ui/lib/es/typography".to_string(),
+    );
+    map.insert(
+        "Upload".to_string(),
+        "@douyinfe/semi-ui/lib/es/upload".to_string(),
+    );
+    map.insert(
         "useFieldApi".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "useFieldState".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "useFormApi".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "useFormState".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "withField".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "withFormApi".to_string(),
+        "@douyinfe/semi-ui/lib/es/form".to_string(),
+    );
+    map.insert(
+        "withFormState".to_string(),
         "@douyinfe/semi-ui/lib/es/form".to_string(),
     );
     map

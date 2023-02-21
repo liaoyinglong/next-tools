aa967bf0910d82ef4614980394de7b588497f03e
liaoyinglong <vigossliao@gmail.com>
2023-02-21 14:42:29 +0800

    wip: 更新测试用例

--- packages/swc_plugin/tests/fixture/semi_ui/ModularizeImports/input.js
+++ packages/swc_plugin/tests/fixture/semi_ui/ModularizeImports/input.js
@@ -1,11 +1,15 @@
 import { Input, Space as SemiUiSpace } from "@douyinfe/semi-ui";
 import { LocaleProvider } from "@douyinfe/semi-ui";
+import { IconHome, IconEmoji, IconSpin } from "@douyinfe/semi-icons";
 
 function App() {
   return (
     <LocaleProvider>
       <Input />
       <SemiUiSpace />
+      <IconHome></IconHome>
+      <IconEmoji></IconEmoji>
+      <IconSpin></IconSpin>
     </LocaleProvider>
   );
 }
--- packages/swc_plugin/tests/fixture/semi_ui/ModularizeImports/output.js
+++ packages/swc_plugin/tests/fixture/semi_ui/ModularizeImports/output.js
@@ -1,3 +1,6 @@
+import IconSpin from "@douyinfe/semi-icons/lib/es/icons/IconSpin";
+import IconEmoji from "@douyinfe/semi-icons/lib/es/icons/IconEmoji";
+import IconHome from "@douyinfe/semi-icons/lib/es/icons/IconHome";
 import LocaleProvider from "@douyinfe/semi-ui/lib/es/locale/localeProvider";
 import SemiUiSpace from "@douyinfe/semi-ui/lib/es/space";
 import Input from "@douyinfe/semi-ui/lib/es/input";
@@ -7,6 +10,12 @@ function App() {
       <Input />
 
       <SemiUiSpace />
+
+      <IconHome ></IconHome>
+
+      <IconEmoji ></IconEmoji>
+
+      <IconSpin ></IconSpin>
 
     </LocaleProvider>;
 }

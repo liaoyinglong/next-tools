2a4fa9645cb74b47a6359f0e155dddbf96e28865
liaoyinglong <vigossliao@gmail.com>
2023-03-31 20:21:38 +0800

    feat: 暴露相关helpers

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,6 @@
 export { Config, defineConfig } from "./shared/config";
+
+export { googleSheet } from "./shared/google/sheet";
+
+export * from "./shared/letters";
+export * from "./shared/resolveSheetId";

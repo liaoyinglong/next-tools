c42fdf5d68a5d0f26612aca5fb651aa9fa86204c
liaoyinglong <vigossliao@gmail.com>
2023-05-30 14:06:06 +0800

    feat: 导出 config 相关

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,5 @@
-export { Config, defineConfig } from "./shared/config";
-
 export { googleSheet } from "./shared/google/sheet";
 
 export * from "./shared/letters";
 export * from "./shared/resolveSheetId";
+export * from "./shared/config";

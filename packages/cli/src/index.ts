3b2682b49301eed545907645071290530b9e7521
liaoyinglong <vigossliao@gmail.com>
2023-06-14 16:32:20 +0800

    feat: 暴露相关接口

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,6 @@
 export { googleSheet } from "./shared/google/sheet";
+export { googleAuth } from "./shared/google/auth";
+export { google } from "googleapis";
 
 export * from "./shared/letters";
 export * from "./shared/resolveSheetId";

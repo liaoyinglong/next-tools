889e3a2160c641da2214cecb380fe8c2d35cb67b
liaoyinglong <vigossliao@gmail.com>
2023-06-16 17:44:05 +0800

    fix: 基于正则实现的 namespace 添加

---
+++ packages/cli/src/shared/autoNamespaceByReg.ts
@@ -0,0 +1,53 @@
+/**
+ * 目前 支持 . 分割的 namespace
+ */
+export function autoNamespaceByReg(code: string, namespace: string) {
+  let hasChanged = false;
+
+  /**
+   * replace case:
+   * t`login` => t`Merchants.login`
+   * t`hello ${name}` => t`Merchants.hello ${name}`
+   */
+  code = code.replace(/t`(.+?)`/gm, (match, p1) => {
+    // 如果 当前 有 $b. 则不替换
+    if (/.+\./.test(p1)) {
+      return match;
+    }
+    hasChanged = true;
+    return `t\`${namespace}.${p1}\``;
+  });
+
+  /**
+   * replace case:
+   * t('login') => t('Merchants.login')
+   * t("login") => t('Merchants.login')
+   */
+  code = code.replace(/t\((["'])(.+?)\1\)/gm, (match, p1, p2) => {
+    // 如果 当前 有 $b. 则不替换
+    if (/.+\./.test(p2)) {
+      return match;
+    }
+    hasChanged = true;
+    return `t('${namespace}.${p2}')`;
+  });
+
+  /**
+   * replace case:
+   * t('login',{ id:'1' }) => t('Merchants.login',{ id:'1' })
+   * t("login",{ id:'1' }) => t('Merchants.login',{ id:'1' })
+   */
+  code = code.replace(/t\((["'])(.+?)\1(,|.*\))/gm, (match, p1, p2, p3) => {
+    // 如果 当前 有 $b. 则不替换
+    if (/.+\./.test(p2)) {
+      return match;
+    }
+    hasChanged = true;
+    return `t('${namespace}.${p2}'${p3}`;
+  });
+
+  if (hasChanged) {
+    return code;
+  }
+  return "";
+}
---
+++ packages/cli/tests/autoNamespaceByReg.test.ts
@@ -0,0 +1,66 @@
+import { describe, it, expect } from "vitest";
+import { autoNamespaceByReg } from "../src/shared/autoNamespaceByReg";
+
+describe("autoNamespaceByReg", () => {
+  it("tpl", function () {
+    expect(
+      autoNamespaceByReg(
+        ["t`login`", "t`hello ${name}`"].join("\n"),
+        "Merchants"
+      )
+    ).toMatchInlineSnapshot(`
+      "t\`Merchants.login\`
+      t\`Merchants.hello \${name}\`"
+    `);
+  });
+  it("normal", () => {
+    expect(
+      autoNamespaceByReg(
+        [
+          't("login")',
+          `t('login')`,
+          `t("login")`,
+          `t('hello {name}', { name: 'world' })`,
+          `t("hello {name}", { name: 'world' })`,
+        ].join("\n"),
+        "Menu"
+      )
+    ).toMatchInlineSnapshot(`
+      "t('Menu.login')
+      t('Menu.login')
+      t('Menu.login')
+      t('Menu.hello {name}', { name: 'world' })
+      t('Menu.hello {name}', { name: 'world' })"
+    `);
+  });
+
+  it("不应该替换已经有 namespace 的", function () {
+    expect(
+      autoNamespaceByReg(
+        [
+          "t`Merchants.login`",
+          `t('Merchants.login')`,
+          't("Merchants.login")',
+          "set`login`",
+          `set('login')`,
+          `set("login")`,
+          //
+          "t`login`",
+          't("login")',
+          `t('login')`,
+        ].join("\n"),
+        "Menu"
+      )
+    ).toMatchInlineSnapshot(`
+      "t\`Merchants.login\`
+      t('Merchants.login')
+      t(\\"Merchants.login\\")
+      set\`Menu.login\`
+      set('Menu.login')
+      set('Menu.login')
+      t\`Menu.login\`
+      t('Menu.login')
+      t('Menu.login')"
+    `);
+  });
+});

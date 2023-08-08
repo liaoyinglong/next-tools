c14dd53bd30cee4e8c0a42a0271a5faee609c1fb
mazaoyong <1204303792@qq.com>
2023-08-08 15:48:48 +0800

    🐞 fix(翻译添加namespace): 修复正则无效的问题

--- packages/cli/src/shared/autoNamespaceByReg.ts
+++ packages/cli/src/shared/autoNamespaceByReg.ts
@@ -9,13 +9,13 @@ export function autoNamespaceByReg(code: string, namespace: string) {
    * t`login` => t`Merchants.login`
    * t`hello ${name}` => t`Merchants.hello ${name}`
    */
-  code = code.replace(/^t`(.+?)`/gm, (match, p1) => {
+  code = code.replace(/\Wt`(.+?)`/gm, (match, p1) => {
     // 如果 当前 有 $b. 则不替换
     if (/.+\./.test(p1)) {
       return match;
     }
     hasChanged = true;
-    return `t\`${namespace}.${p1}\``;
+    return match.replace(p1, `${namespace}.${p1}`);
   });
 
   /**
@@ -23,28 +23,29 @@ export function autoNamespaceByReg(code: string, namespace: string) {
    * t('login') => t('Merchants.login')
    * t("login") => t('Merchants.login')
    */
-  code = code.replace(/^t\((["'])(.+?)\1\)/gm, (match, p1, p2) => {
+  code = code.replace(/\Wt\(['"](.+?)['"][),]/gm, (match, p1) => {
     // 如果 当前 有 $b. 则不替换
-    if (/.+\./.test(p2)) {
+    if (/.+\./.test(p1)) {
       return match;
     }
     hasChanged = true;
-    return `t('${namespace}.${p2}')`;
+    return match.replace(p1, `${namespace}.${p1}`);
   });
 
   /**
+   * 第二个方法已经满足这种情况了，先注释掉
    * replace case:
    * t('login',{ id:'1' }) => t('Merchants.login',{ id:'1' })
    * t("login",{ id:'1' }) => t('Merchants.login',{ id:'1' })
    */
-  code = code.replace(/^t\((["'])(.+?)\1(,|.*\))/gm, (match, p1, p2, p3) => {
-    // 如果 当前 有 $b. 则不替换
-    if (/.+\./.test(p2)) {
-      return match;
-    }
-    hasChanged = true;
-    return `t('${namespace}.${p2}'${p3}`;
-  });
+  // code = code.replace(/\Wt\((["'])(.+?)\1(,|.*\))/gm, (match, p1, p2, p3) => {
+  //   // 如果 当前 有 $b. 则不替换
+  //   if (/.+\./.test(p2)) {
+  //     return match;
+  //   }
+  //   hasChanged = true;
+  //   return `t('${namespace}.${p2}'${p3}`;
+  // });
 
   if (hasChanged) {
     return code;
--- packages/cli/tests/autoNamespaceByReg.test.ts
+++ packages/cli/tests/autoNamespaceByReg.test.ts
@@ -2,67 +2,70 @@ import { describe, expect, it } from "vitest";
 import { autoNamespaceByReg } from "../src/shared/autoNamespaceByReg";
 
 describe("autoNamespaceByReg", () => {
-  it("tpl", function () {
+  it("反引号", function () {
     expect(
       autoNamespaceByReg(
-        ["t`login`", "t`hello ${name}`"].join("\n"),
+        ["{t`login`}", " t`login`", "(t`hello ${name}`)"].join("\n"),
         "Merchants"
       )
     ).toMatchInlineSnapshot(`
-      "t\`Merchants.login\`
-      t\`Merchants.hello \${name}\`"
+      "{t\`Merchants.login\`}
+       t\`Merchants.login\`
+      (t\`Merchants.hello \${name}\`)"
     `);
   });
-  it("normal", () => {
+
+  it("引号", function () {
     expect(
       autoNamespaceByReg(
-        [
-          't("login")',
-          `t('login')`,
-          `t("login")`,
-          `t('hello {name}', { name: 'world' })`,
-          `t("hello {name}", { name: 'world' })`,
-        ].join("\n"),
-        "Menu"
+        ["{t('引号')}", `(t("引号"))`].join("\n"),
+        "Merchants"
       )
     ).toMatchInlineSnapshot(`
-      "t('Menu.login')
-      t('Menu.login')
-      t('Menu.login')
-      t('Menu.hello {name}', { name: 'world' })
-      t('Menu.hello {name}', { name: 'world' })"
+      "{t('Merchants.引号')}
+      (t(\\"Merchants.引号\\"))"
     `);
   });
+
+  it("对象入参", function () {
+    expect(
+      autoNamespaceByReg(
+        ["{t('hello: {name}',{ name:'zhangsan' })}"].join("\n"),
+        "Merchants"
+      )
+    ).toMatchInlineSnapshot('"{t(\'Merchants.hello: {name}\',{ name:\'zhangsan\' })}"');
+  })
 
   it("不应该替换已经有 namespace 的", function () {
     expect(
       autoNamespaceByReg(
         [
-          "t`Merchants.login`",
-          `t('Merchants.login')`,
-          't("Merchants.login")',
+          " t`Merchants.login`",
+          ` t('Merchants.login')`,
+          ' t("Merchants.login")',
           "set`login`",
           `set('login')`,
           `set("login")`,
           //
-          "t`login`",
-          't("login")',
-          `t('login')`,
+          " t`login`",
+          ' t("login")',
+          ` t('login')`,
           `arr.split('login')`,
         ].join("\n"),
         "Menu"
       )
     ).toMatchInlineSnapshot(`
-      "t\`Merchants.login\`
-      t('Merchants.login')
-      t(\\"Merchants.login\\")
+      " t\`Merchants.login\`
+       t('Merchants.login')
+       t(\\"Merchants.login\\")
       set\`login\`
       set('login')
       set(\\"login\\")
-      t\`Menu.login\`
-      t('Menu.login')
-      t('Menu.login')
+       t\`Menu.login\`
+       t(\\"Menu.login\\")
+       t('Menu.login')
       arr.split('login')"
     `);
   });
+
 });

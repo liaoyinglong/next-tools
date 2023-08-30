9f7e518253141cf5ef5a1017f8176380b0173c0f
liaoyinglong <vigossliao@gmail.com>
2023-08-30 11:38:19 +0800

    feat: mapProps 支持 纯对象参数当做默认值

--- packages/tools/src/factory/mapProps.tsx
+++ packages/tools/src/factory/mapProps.tsx
@@ -12,14 +12,25 @@ import { forwardRef } from "react";
 export function mapProps<
   C extends keyof JSX.IntrinsicElements,
   P extends JSX.IntrinsicElements[C]
->(BaseComponent: C, mapper: (p: P) => P): FC<P>;
+>(BaseComponent: C, mapper: P | ((p: P) => P)): FC<P>;
 export function mapProps<
   C extends ComponentType<any>,
   P extends ComponentProps<C>
->(BaseComponent: C, mapper: (p: P) => P): FC<P>;
-export function mapProps<C, P>(BaseComponent: any, mapper: (p: P) => P) {
+>(BaseComponent: C, mapper: P | ((p: P) => P)): FC<P>;
+export function mapProps<C, P extends Record<any, any>>(
+  BaseComponent: any,
+  mapper: P | ((p: P) => P)
+) {
   const MappedComponent = (props: P, ref: any) => {
-    return <BaseComponent {...mapper(props)} ref={ref}></BaseComponent>;
+    let newProps =
+      typeof mapper === "function"
+        ? mapper(props)
+        : {
+            ...mapper,
+            ...props,
+          };
+
+    return <BaseComponent {...newProps} ref={ref}></BaseComponent>;
   };
   if (process.env.NODE_ENV !== "production") {
     MappedComponent.displayName = `mapProps(${getDisplayName(BaseComponent)})`;

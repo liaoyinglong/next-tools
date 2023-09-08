ed7f859e0012b8fa7795d2ae2a4f272d59cc5bfa
liaoyinglong <vigossliao@gmail.com>
2023-09-08 23:03:06 +0800

    docs: wip

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,8 +1,8 @@
 // @see https://nextra.site/docs/docs-theme/theme-configuration
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>dune Documentation</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://bitbucket.org/liaoyinglong/next-tools",
   },
   // ... other theme options
 };

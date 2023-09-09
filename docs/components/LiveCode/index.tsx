403c488bb5add375ff1844490dc7c38eca2a2ef7
liaoyinglong <vigossliao@gmail.com>
2023-09-10 00:54:51 +0800

    docs: createStateContext

--- docs/components/LiveCode/index.tsx
+++ docs/components/LiveCode/index.tsx
@@ -4,4 +4,14 @@ import { mapProps } from "@dune2/tools";
 export const LiveCode = mapProps(Sandpack, {
   theme: "auto",
   template: "react",
+  options: {
+    showInlineErrors: true, // default - false
+    wrapContent: true, // default - false
+    editorHeight: "auto", // default - 300
+  },
+  customSetup: {
+    dependencies: {
+      "@dune2/tools": "latest",
+    },
+  },
 });
--- docs/pages/tools/examples/createStateContext/App.js
+++ docs/pages/tools/examples/createStateContext/App.js
@@ -1,3 +1,32 @@
+import { createStateContext } from "@dune2/tools/src/factory/createStateContext";
+import { useState } from "react";
+
+const { Provider, useContextValue } = createStateContext({
+  useValueHooks() {
+    const [count, setCount] = useState(0);
+    return {
+      count,
+      increment() {
+        setCount(count + 1);
+      },
+    };
+  },
+});
+
+function ChildA() {
+  const { increment } = useContextValue();
+  return <button onClick={increment}>increment</button>;
+}
+function ChildB() {
+  const { count } = useContextValue();
+  return <div>count: {count}</div>;
+}
+
 export default function App() {
-  return <div>createStateContext</div>;
+  return (
+    <Provider>
+      <ChildA></ChildA>
+      <ChildB></ChildB>
+    </Provider>
+  );
 }

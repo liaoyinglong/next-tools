40226413f705c06948da508ba3c55708b3999c9f
liaoyinglong <vigossliao@gmail.com>
2023-09-10 10:42:22 +0800

    fix: sandPack not need ssr

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,18 +1,7 @@
-import { getSandpackCssText } from "@codesandbox/sandpack-react";
-
 // @see https://nextra.site/docs/docs-theme/theme-configuration
 export default {
   logo: <span>dune Documentation</span>,
   project: {
     link: "https://bitbucket.org/liaoyinglong/next-tools",
-  },
-  head() {
-    return (
-      <style
-        dangerouslySetInnerHTML={{ __html: getSandpackCssText() }}
-        id="sandpack"
-        key="sandpack-css"
-      />
-    );
   },
 };

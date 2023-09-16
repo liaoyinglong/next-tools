cac837ea9d17c65748b692b0e9d16e413e387486
liaoyinglong <vigossliao@gmail.com>
2023-09-16 20:18:14 +0800

    wip: 添加 radix

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,13 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+
 // @see https://nextra.site/docs/docs-theme/theme-configuration
 export default {
   logo: <span>dune Documentation</span>,
   project: {
     link: "https://bitbucket.org/liaoyinglong/next-tools",
+  },
+  main: (props) => {
+    return <Theme>{props.children}</Theme>;
   },
 };

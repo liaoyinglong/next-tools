c9a3848d9e5ce38e392398cb307773278b8f60f1
liaoyinglong <vigossliao@gmail.com>
2023-09-17 19:20:04 +0800

    移除 radix themes

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,13 +1,10 @@
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-
 // @see https://nextra.site/docs/docs-theme/theme-configuration
 export default {
   logo: <span>dune Documentation</span>,
   project: {
     link: "https://bitbucket.org/liaoyinglong/next-tools",
   },
-  main: (props) => {
-    return <Theme>{props.children}</Theme>;
-  },
+  // main: (props) => {
+  //   return <Theme>{props.children}</Theme>;
+  // },
 };

147b7f84bee6795b995ec2705eb2861b63c88ac9
liaoyinglong <vigossliao@gmail.com>
2023-09-18 10:30:56 +0800

    wip: docs build

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,8 +3,18 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
+let basePath = "/";
+//#region in github action build
+if (process.env.GITHUB_REPOSITORY) {
+  // eg: GITHUB_REPOSITORY = "dune2/some-tools"
+  basePath = `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`;
+  console.log(`basePath: ${basePath}`);
+}
+
+//#endregion
 
 module.exports = withNextra({
+  basePath,
   transpilePackages: ["@dune2/tools"],
   output: "export",
   typescript: {

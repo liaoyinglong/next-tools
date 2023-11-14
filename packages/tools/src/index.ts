ca3e2390f492d02be42724fd33dd6738a66b8b99
liaoyinglong <vigossliao@gmail.com>
2023-11-14 17:27:04 +0800

    feat: 添加 shared 导出

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -6,3 +6,12 @@ export * from "../rq/RequestBuilder";
 export * from "../rq/defaultQueryClient";
 export * from "../storage";
 export * from "../storage/cookie";
+
+if (process.env.NODE_ENV === "development") {
+  console.trace(
+    [
+      `下个版本将移除barrel文件，使用单独的导出文件。`,
+      "修改 `import {} from '@dune2/tools'` 为 `import {} from '@dune2/tools/xxx'` ",
+    ].join("\n")
+  );
+}

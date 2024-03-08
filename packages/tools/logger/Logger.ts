38eec1340edbe5a1bf6c63074b430e70405dfebb
liaoyinglong <vigossliao@gmail.com>
2024-03-08 17:24:14 +0800

    Update Logger.ts and shared.ts

--- packages/tools/logger/Logger.ts
+++ packages/tools/logger/Logger.ts
@@ -1,21 +1,7 @@
 import _ from "lodash";
 import store2 from "store2";
-import { Level } from "./shared";
+import { Level, type OnLogParams } from "./shared";
 
-type OnLogParams = {
-  // 日志等级
-  level: Level;
-  // 可读的日志等级
-  tag: string;
-  // 用于标识日志的名称
-  name: string;
-  // 日志输出时间
-  time: number;
-  // 是否应该输出日志
-  shouldLog: boolean;
-  // 日志内容
-  msg: any[];
-};
 type StorageState = Record<string, Level>;
 
 export type OnLog = (data: OnLogParams) => void;
--- packages/tools/logger/shared.ts
+++ packages/tools/logger/shared.ts
@@ -5,3 +5,19 @@ export enum Level {
   Error,
   Silent,
 }
+
+
+export type OnLogParams = {
+  // 日志等级
+  level: Level;
+  // 可读的日志等级
+  tag: string;
+  // 用于标识日志的名称
+  name: string;
+  // 日志输出时间
+  time: number;
+  // 是否应该输出日志
+  shouldLog: boolean;
+  // 日志内容
+  msg: any[];
+};

a5015a89c5a4709a4150d4af90fadf584af4665f
liaoyinglong <vigossliao@gmail.com>
2024-06-11 10:38:39 +0800

    lazy load command

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,65 +1,91 @@
 #!/usr/bin/env node
 import { version } from "../package.json";
-import { download } from "./commands/download";
-import { downloadFromPlatform } from "./commands/downloadFromPlatform";
-import { extract } from "./commands/extract";
-import { generateApi } from "./commands/generateApi";
-import { initConfig } from "./commands/initConfig";
-import { interactive } from "./commands/interactive";
-import { namespace } from "./commands/namespace";
-import { upload } from "./commands/upload";
 import { cli } from "./shared";
 //#region 翻译相关
 
 cli
   .command("download", "生成翻译文件")
   .example("dune download")
-  .action(download);
+  .action(async () => {
+    const { download } = await import("./commands/download");
+    await download();
+  });
 cli
   .command("extract", "提取代码中的文案")
   .option("--deleteUnused", "删除未使用的文案")
   .example("dune extract")
-  .action(extract);
+  .action(async (args) => {
+    const { extract } = await import("./commands/extract");
+    await extract(args);
+  });
 
 cli
-  .command("namespaceSwc", "添加namespace前缀，由swc驱动")
+  .command("namespaceSwc", "添加 namespace 前缀，由 swc 驱动")
   .example("dune namespaceSwc")
-  .action(() => {
+  .action(async () => {
+    const { namespace } = await import("./commands/namespace");
     return namespace({ mode: "swc" });
   });
 
 cli
-  .command("namespaceReg", "添加namespace前缀，由正则驱动")
+  .command("namespaceReg", "添加 namespace 前缀，由正则驱动")
   .example("dune namespaceReg")
-  .action(() => {
+  .action(async () => {
+    const { namespace } = await import("./commands/namespace");
     return namespace({ mode: "reg" });
   });
 
-cli.command("upload", "上传翻译文件").example("dune upload").action(upload);
+cli
+  .command("upload", "上传翻译文件")
+  .example("dune upload")
+  .action(async (args) => {
+    const { upload } = await import("./commands/upload");
+    await upload();
+  });
 
 cli
   .command("downloadFromPlatform", "从翻译平台下载文件")
   .example("dune downloadFromPlatform")
-  .action(downloadFromPlatform);
+  .action(async () => {
+    const { downloadFromPlatform } = await import(
+      "./commands/downloadFromPlatform"
+    );
+    await downloadFromPlatform();
+  });
 //#endregion
 
 //#region api 相关
 cli
-  .command("generateApi", "生成api文件")
+  .command("generateApi", "生成 api 文件")
   .example("dune generateApi")
-  .action(generateApi);
+  .action(async () => {
+    const { generateApi } = await import("./commands/generateApi");
+    await generateApi();
+  });
 //#endregion
 
-cli.command("init", "初始化配置文件").example("dune init").action(initConfig);
+cli
+  .command("init", "初始化配置文件")
+  .example("dune init")
+  .action(async () => {
+    const { initConfig } = await import("./commands/initConfig");
+    await initConfig();
+  });
 
 cli
   .command("interactive", "交互式操作")
   .example("dune interactive")
   .alias("i")
-  .action(interactive);
+  .action(async (args) => {
+    const { interactive } = await import("./commands/interactive");
+    await interactive(args);
+  });
 
 // make default command run interactive
-cli.command("").action(interactive);
+cli.command("").action(async (args) => {
+  const { interactive } = await import("./commands/interactive");
+  await interactive(args);
+});
 
 cli.version(version);
 cli.help();

0fb8ce4ead958e1184e18257cf320f87cf2ce213
liaoyinglong <vigossliao@gmail.com>
2024-07-22 23:36:11 +0800

    update test

--- packages/tools/tests/rq/query.test-d.ts
+++ packages/tools/tests/rq/query.test-d.ts
@@ -48,7 +48,24 @@ describe("rq.query", () => {
   });
 
   it("use query return type", () => {
-    const res = authOauthTokenPostApi.useQuery(req);
-    assertType<authOauthTokenPostApi.Res | undefined>(res.data);
+    {
+      // 默认 res
+      const res = authOauthTokenPostApi.useQuery(req);
+      assertType<authOauthTokenPostApi.Res | undefined>(res.data);
+    }
+    {
+      // 特殊 res
+      type Res = {
+        userId: number;
+      };
+      const res = authOauthTokenPostApi.useQuery<Res>(req, {
+        select(data) {
+          return {
+            userId: data.userId,
+          };
+        },
+      });
+      assertType<Res | undefined>(res.data);
+    }
   });
 });

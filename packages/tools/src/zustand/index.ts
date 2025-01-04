992f4ad47ec6c74fcb07c92861d1fc1f0fb6c774
liaoyinglong <vigossliao@gmail.com>
2025-01-04 23:25:38 +0800

    No changes detected in the current diff. Please verify the modifications before committing.

---
+++ packages/tools/src/zustand/index.ts
@@ -0,0 +1,56 @@
+import { produce } from "immer";
+import { create } from "zustand";
+type State<S extends object, A extends Record<string, Action<S, any>>> = {
+  state: S;
+  actions: A;
+};
+
+export function createStore<
+  S extends object,
+  A extends Record<string, Action<S, any>>,
+>(config: State<S, A>) {
+  const useStore = create<S>((set) => {
+    return config.state;
+  });
+  type NormalActions = {
+    [key in keyof A]: (payload: Parameters<A[key]>[1]) => void;
+  };
+  const normalActions: NormalActions = {} as NormalActions;
+  Object.keys(config.actions).forEach((key) => {
+    // @ts-ignore
+    normalActions[key] = (payload) => {
+      useStore.setState(
+        produce((draft) => {
+          config.actions[key](draft, payload);
+        }),
+        // 尽可能复用 immer 更新的数据
+        true,
+      );
+    };
+  });
+
+  return {
+    /**
+     * 在组件内获取状态
+     */
+    useSnapshot: useStore,
+    /**
+     * 普通方法，设置状态
+     */
+    actions: normalActions,
+    /**
+     * 监听状态变化
+     */
+    subscribe: useStore.subscribe,
+    /**
+     * 获取状态
+     */
+    getState: useStore.getState,
+    /**
+     * 设置状态
+     */
+    setState: useStore.setState,
+  };
+}
+
+type Action<S, T> = (state: S, payload: T) => any;
---
+++ packages/tools/tests/zustand/index.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { createStore } from "../../src/zustand";
+
+describe("zustand", () => {
+  it("normal actions should work", () => {
+    const store = createStore({
+      state: { a: 1, b: 2, c: { name: "hello" }, d: { name: "world" } },
+      actions: {
+        setA: (s, b: number) => {
+          s.a = 123;
+        },
+        setB: (s, b: number) => {
+          s.b = 123;
+        },
+        setC: (s, name: string) => {
+          s.c.name = name;
+        },
+      },
+    });
+
+    const oldState = store.getState();
+    // 更新 a / b ，c 的引用和值不会变
+    store.actions.setA(123);
+    expect(store.getState().a).toBe(123);
+    expect(store.getState().c).toBe(oldState.c);
+    expect(store.getState().c).toEqual({ name: "hello" });
+
+    // 更新 c ，c 的引用和值都会变 但是 d 的引用和值不会变
+    store.actions.setC("abc");
+    expect(store.getState().c).not.toBe(oldState.c);
+    expect(store.getState().c).toEqual({ name: "abc" });
+    expect(store.getState().d).toBe(oldState.d);
+    expect(store.getState().d).toEqual({ name: "world" });
+  });
+});

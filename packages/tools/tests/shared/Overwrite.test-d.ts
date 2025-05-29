fecfd3906c76602ab5e98d62772ce79a5f625c7f
liaoyinglong <vigossliao@gmail.com>
2025-05-29 10:31:27 +0400

    add overwrite

---
+++ packages/tools/tests/shared/Overwrite.test-d.ts
@@ -0,0 +1,19 @@
+import { assertType, describe, it } from "vitest";
+import type { Overwrite } from "../../src/shared/Overwrite";
+
+describe("Overwrite", () => {
+  it("should be ok", () => {
+    type A = {
+      name: string;
+      age: number;
+      work?: string;
+    };
+    type B = {
+      name: number;
+    };
+    type C = Overwrite<A, B>;
+    //   ^? { name: number; age: number; work?: string; }
+
+    assertType<C>({ name: 1, age: 1, work: "1" });
+  });
+});

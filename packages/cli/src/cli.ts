8f2dd59db1ab43ad521d752c8f27c11ce22278f7
liaoyinglong <vigossliao@gmail.com>
2025-06-12 09:56:17 +0400

    remove unused function

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,49 +1,6 @@
 #!/usr/bin/env node
 import { version } from "../package.json";
 import { cli } from "./shared";
-//#region 翻译相关
-
-cli
-  .command("download", "生成翻译文件")
-  .example("dune download")
-  .action(async () => {
-    const { download } = await import("./commands/download");
-    await download();
-  });
-cli
-  .command("extract", "提取代码中的文案")
-  .option("--deleteUnused", "删除未使用的文案")
-  .example("dune extract")
-  .action(async (args) => {
-    const { extract } = await import("./commands/extract");
-    await extract(args);
-  });
-
-cli
-  .command("namespaceSwc", "添加 namespace 前缀，由 swc 驱动")
-  .example("dune namespaceSwc")
-  .action(async () => {
-    const { namespace } = await import("./commands/namespace");
-    return namespace({ mode: "swc" });
-  });
-
-cli
-  .command("namespaceReg", "添加 namespace 前缀，由正则驱动")
-  .example("dune namespaceReg")
-  .action(async () => {
-    const { namespace } = await import("./commands/namespace");
-    return namespace({ mode: "reg" });
-  });
-
-cli
-  .command("upload", "上传翻译文件")
-  .example("dune upload")
-  .action(async (args) => {
-    const { upload } = await import("./commands/upload");
-    await upload();
-  });
-
-//#endregion
 
 //#region api 相关
 cli
@@ -53,30 +10,6 @@ cli
     const { generateApi } = await import("./commands/generateApi");
     await generateApi();
   });
-//#endregion
-
-//#region user 相关
-cli.command("login", "登录").action(async () => {
-  const { googleAuth } = await import("./shared/google/auth");
-  await googleAuth.initCredentials();
-});
-cli.command("logout", "退出").action(async () => {
-  const { googleAuth } = await import("./shared/google/auth");
-  await googleAuth.removeCredentials();
-});
-cli.command("userInfo", "用户信息").action(async () => {
-  const { googleAuth } = await import("./shared/google/auth");
-  if (googleAuth.tokens) {
-    console.log(`\
-email: ${googleAuth.tokens.email}
-aud  : ${googleAuth.tokens.aud}
-exp  : ${new Date(googleAuth.tokens.expiry_date).toLocaleString()}
-`);
-  } else {
-    console.log("未登录");
-    console.log(`运行 dune login 登录`);
-  }
-});
 //#endregion
 
 cli

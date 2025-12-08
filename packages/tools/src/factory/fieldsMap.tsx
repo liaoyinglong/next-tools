8816f9ac71560800cfa98fda2b86ac79bead498d
liaoyinglong <vigossliao@gmail.com>
2025-12-08 12:18:28 +0800

    feat(factory): add fallback for fieldsMap when Proxy is undefined
    Added a check to handle environments where Proxy is unavailable. Logs a warning and uses an empty object as fieldsMap to prevent runtime errors and improve compatibility.

--- packages/tools/src/factory/fieldsMap.tsx
+++ packages/tools/src/factory/fieldsMap.tsx
@@ -5,23 +5,29 @@
  * 使用场景：
  *  - 在字段映射、动态生成键值等需要自动获取属性名的场景中，避免手动硬编码。
  */
-export const fieldsMap = new Proxy(
-  // 目标对象，这里使用一个空对象作为代理的基础
-  {},
-  {
-    /**
-     * 拦截对象的属性读取操作，并返回属性名的字符串形式。
-     *
-     * @param target 原始被代理的对象（此处为空对象）
-     * @param p      被访问的属性名（可能是 string 或 symbol）
-     * @param receiver Proxy 接收者
-     * @returns 属性名的字符串表示
-     */
-    get(target, p, receiver) {
-      return String(p);
-    },
-  },
-) as any;
+export const fieldsMap =
+  typeof Proxy === undefined
+    ? (() => {
+        console.error("Proxy is undefined, using empty object as fieldsMap");
+        return {};
+      })()
+    : (new Proxy(
+        // 目标对象，这里使用一个空对象作为代理的基础
+        {},
+        {
+          /**
+           * 拦截对象的属性读取操作，并返回属性名的字符串形式。
+           *
+           * @param target 原始被代理的对象（此处为空对象）
+           * @param p      被访问的属性名（可能是 string 或 symbol）
+           * @param receiver Proxy 接收者
+           * @returns 属性名的字符串表示
+           */
+          get(target, p, receiver) {
+            return String(p);
+          },
+        },
+      ) as any);
 
 export type FieldsMap<T> =
   T extends Record<string, any>

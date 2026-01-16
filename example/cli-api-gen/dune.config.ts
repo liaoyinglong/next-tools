9599280bc4ec24f34d0a9aee58562795dc0b04d7
liaoyinglong <vigossliao@gmail.com>
2026-01-16 15:12:10 +0800

    update test demo

--- example/cli-api-gen/dune.config.ts
+++ example/cli-api-gen/dune.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from "@dune2/cli";
 export default defineConfig({
   api: [
     {
-      output: "./src/apis",
+      output: "./src/apis/example",
       swaggerJSONPath: "./src/swagger/example.json",
+      swaggerUiUrl: "http://localhost:3000/swagger",
+    },
+    {
+      output: "./src/apis/wma",
+      swaggerJSONPath: "./src/swagger/wma.json",
       swaggerUiUrl: "http://localhost:3000/swagger",
     },
   ],
